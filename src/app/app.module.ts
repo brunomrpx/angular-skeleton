9a154397524472dd5d08edf35f30a6f809500eb5
Bruno Paixão <brunomrpx@gmail.com>
2017-09-25 15:25:15 -0300

    Added initial structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { CoreModule } from 'core/core.module';
+import { PagesModule } from 'pages/pages.module';
+import { DomainModule } from 'domain/domain.module';
+
+import { AppComponent } from 'app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    DomainModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/domain/domain.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from 'domain/shared/shared.module';
+
+@NgModule({
+  imports: [
+    SharedModule
+  ],
+  declarations: [],
+  providers: [],
+})
+export class DomainModule { }
